822f9a526ff0891eb61aaa09e1b379038cb96eed
tsiliopoulos <tsiliopoulos@hotmail.com>
2014-12-04 09:11:50 -0500

    Adjusted states

---
+++ Starbase12-v2/Scripts/states/gameover.js
@@ -0,0 +1,92 @@
+﻿var states;
+(function (states) {
+    function GameOverState() {
+    }
+    states.GameOverState = GameOverState;
+
+    // Restart Game when Try Again Button is clicked
+    function playAgainClicked(event) {
+        stage.removeChild(game);
+        game.removeAllChildren();
+        game.removeAllEventListeners();
+        currentState = config.PLAY_STATE;
+        changeState(currentState);
+    }
+    states.playAgainClicked = playAgainClicked;
+
+    // Read High Score from File
+    /*export function readHighScore() {
+    var xhr: XMLHttpRequest = new XMLHttpRequest();
+    
+    xhr.open("post", "Scripts/scores.txt", false);
+    xhr.send(null);
+    scoreboard.highScore = parseInt(xhr.responseText);
+    }*/
+    // Write High Score to File via PHP
+    /*export function writeHighScore() {
+    var hiScore = new FormData();
+    
+    hiScore.append("data", scoreboard.score.toString());
+    scoreboard.highScore = scoreboard.score;
+    var xhr: XMLHttpRequest = new XMLHttpRequest();
+    xhr.open("post", "Scripts/writeScore.php", true);
+    xhr.send(hiScore);
+    }*/
+    // Game Over Scene
+    function GameOver() {
+        var gameOverLabel;
+        var finalScoreLabel;
+        var finalScore;
+        var highScoreLabel;
+        var highScore;
+        var highScoreString = "";
+        var playAgainButton;
+
+        // Enable Mouse Events
+        stage.enableMouseOver(20);
+
+        // Declare new Game Container
+        game = new createjs.Container();
+
+        // Show Cursor
+        //stage.cursor = "default";
+        // Check if player beat high score
+        //readHighScore();
+        /*if (scoreboard.score > scoreboard.highScore) {
+        writeHighScore();
+        }
+        highScoreString = hud.highScore.toString();*/
+        // Display Game Over
+        gameOverLabel = new objects.Label(config.MIDDLE_X, 100, "Game Over");
+        gameOverLabel.fontSize(70);
+        game.addChild(gameOverLabel);
+
+        // Display Final Score Label
+        finalScoreLabel = new objects.Label(config.MIDDLE_X, 180, "Your Score");
+        finalScoreLabel.fontSize(60);
+        game.addChild(finalScoreLabel);
+
+        // Display Final Score
+        finalScore = new objects.Label(config.MIDDLE_X, 280, Math.floor(hud.score).toString());
+        finalScore.fontSize(60);
+        game.addChild(finalScore);
+
+        /* // Display High Score Label
+        highScoreLabel = new objects.Label(config.MIDDLE_X, 240, "High Score");
+        highScoreLabel.fontSize(40);
+        game.addChild(highScoreLabel);
+        
+        // Display High Score
+        highScore = new objects.Label(config.MIDDLE_X, 300, highScoreString);
+        highScore.fontSize(40);
+        game.addChild(highScore);*/
+        // Display Try Again Button
+        playAgainButton = new objects.Button(config.MIDDLE_X, 360, "playAgainButton");
+        game.addChild(playAgainButton);
+        playAgainButton.addEventListener("click", playAgainClicked);
+
+        stage.addChild(game);
+    }
+    states.GameOver = GameOver;
+})(states || (states = {}));
+//# sourceMappingURL=gameover.js.map
